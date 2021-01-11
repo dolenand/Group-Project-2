a08461e9fcd06edd24a13afa38b67d8327666982
emily <emily.a.h@hotmail.com>
2021-01-11 12:20:42 -0500

    Fixed data loading for index and chart

--- controllers/finances_controller.js
+++ controllers/finances_controller.js
@@ -36,6 +36,13 @@ router.get("/accounts", function(req, res) {
   });
 });
 
+// Use to populate expense data on page load
+router.get("/api/expenses", function(req, res) {
+  finances.allExpenses(function(data) {
+    res.json(data);
+  });
+});
+
 router.post("/api/expenses", function(req, res) {
   finances.createExpenses([
     "expense_type", "ex_amount"
